2fd4bfb447c885ffef64c6cc448f04a07a1fd952
Al Armato <55386215+AleArmato@users.noreply.github.com>
2021-09-25 15:26:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let client = null;
+
+function connect() {
+ let brokerURL = document.getElementById("brokerURL").value;
+ let port = parseInt(document.getElementById("port").value);
+ let date = new Date().toLocaleString().replace(",","");
+ updateMessages(date + " | Connecting to " + brokerURL + " on port " + port);
+ client = new Paho.Client(brokerURL, port, "");
+ client.onConnectionLost = onConnectionLost;
+ client.onMessageArrived = onMessageArrived;
+ client.connect({onSuccess: onConnect, useSSL: true});
+}
+
+function onConnect() {
+ let topic = document.getElementById("topic").value;
+ let date = new Date().toLocaleString().replace(",","");
+ updateMessages(date + " | Subscribing to topic " + topic);
+ client.subscribe(topic);
+ date = new Date().toLocaleString().replace(",","");
+ updateMessages(date + " | Connected");
+ document.getElementById("disconnect").disabled = false;
+ checkText();
+}
+
+function disconnect() {
+ client.disconnect();
+ let date = new Date().toLocaleString().replace(",","");
+ updateMessages(date + " | Disconnected");
+ document.getElementById("disconnect").disabled = true;
+ document.getElementById("send").disabled = true;
+}
+
+function onConnectionLost(message) {
+ if(message.errorCode !== 0) {
+  let date = new Date().toLocaleString().replace(",","");
+  updateMessages(date + " | Connection lost: " + message.errorMessage);
+  document.getElementById("disconnect").disabled = true;
+  document.getElementById("send").disabled = true;
+ }
+}
+
+function onMessageArrived(message) {
+ let date = new Date().toLocaleString().replace(",","");
+ updateMessages(date + " | " + message.destinationName + " | " + message.payloadString);
+}
+
+function updateMessages(message) {
+ let messages = document.getElementById("messages").value !== "" ? document.getElementById("messages").value + "\n" : document.getElementById("messages").value;
+ document.getElementById("messages").value = messages + message;
+}
+
+function sendMessage() {
+ let topic = document.getElementById("topic").value;
+ let username = document.getElementById("username").value.trim() !== "" ? document.getElementById("username").value.trim() : "nousername";
+ let message = username + ":\n" + document.getElementById("message").value.trim();
+ document.getElementById("message").value = "";
+ document.getElementById("send").disabled = true;
+ client.publish(topic, message, 1, false);
+}
+
+function checkText() {
+ let message = document.getElementById("message").value.trim();
+ if(message !== "" && !document.getElementById("disconnect").disabled) {
+   document.getElementById("send").disabled = false;
+ }
+ else {
+   document.getElementById("send").disabled = true;
+ }
+}
