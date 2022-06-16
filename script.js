fcd9786d58ff7e7611345940c1d45a10e6e927fd
zyzalfors <ale.armato@libero.it>
2022-06-16 21:44:58 +0200

    update to v1.0.8

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let client = null;
 let topic = null;
 
-function connect() {
+function connect()
+{
  let brokerURL = document.getElementById("brokerURL").value.trim();
  let port = parseInt(document.getElementById("port").value.trim());
  topic = document.getElementById("topic").value.trim();
  let date = new Date().toLocaleString().replace(",", "");
- if(brokerURL === "" || isNaN(port) || topic === "") {
+ if(brokerURL === "" || isNaN(port) || topic === "")
+ {
   updateMessages(date + " | Invalid broker URL, port or topic", "logs");
   return;
  }
@@ -17,7 +19,8 @@ function connect() {
  client.connect({onSuccess: onSuccess, onFailure: onFailure, useSSL: true});
 }
 
-function onSuccess() {
+function onSuccess()
+{
  let date = new Date().toLocaleString().replace(",", "");
  updateMessages(date + " | Subscribing to topic " + topic, "logs");
  client.subscribe(topic);
@@ -28,12 +31,14 @@ function onSuccess() {
  checkText();
 }
 
-function onFailure(obj) {
+function onFailure(obj)
+{
  let date = new Date().toLocaleString().replace(",", "");
  updateMessages(date + " | " + obj.errorMessage, "logs");
 }
 
-function disconnect() {
+function disconnect()
+{
  client.disconnect();
  let date = new Date().toLocaleString().replace(",", "");
  updateMessages(date + " | Disconnected", "logs");
@@ -42,8 +47,10 @@ function disconnect() {
  document.getElementById("send").disabled = true;
 }
 
-function onConnectionLost(message) {
- if(message.errorCode !== 0) {
+function onConnectionLost(message)
+{
+ if(message.errorCode !== 0)
+ {
   let date = new Date().toLocaleString().replace(",", "");
   updateMessages(date + " | Connection lost: " + message.errorMessage, "logs");
   document.getElementById("disconnect").disabled = true;
@@ -52,22 +59,26 @@ function onConnectionLost(message) {
  }
 }
 
-function onMessageArrived(message) {
+function onMessageArrived(message)
+{
  let date = new Date().toLocaleString().replace(",", "");
  updateMessages(date + " | " + message.destinationName + " | " + message.payloadString, "messages");
 }
 
-function updateMessages(message, textAreaID) {
+function updateMessages(message, textAreaID)
+{
  let textarea = document.getElementById(textAreaID);
  let messages = textarea.value !== "" ? textarea.value + "\n" : textarea.value;
  textarea.value = messages + message;
  textarea.scrollTop = textarea.scrollHeight;
- if(textAreaID === "messages") {
+ if(textAreaID === "messages")
+ {
   document.getElementById("clear").disabled = false;
  }
 }
 
-function sendMessage() {
+function sendMessage()
+{
  let username = document.getElementById("username").value.trim() !== "" ? document.getElementById("username").value.trim() : "nousername";
  let message = username + ":\n" + document.getElementById("message").value.trim();
  document.getElementById("message").value = "";
@@ -75,24 +86,30 @@ function sendMessage() {
  client.publish(topic, message, 1, false);
 }
 
-function checkText() {
+function checkText()
+{
  let message = document.getElementById("message").value.trim();
- if(message !== "" && document.getElementById("connect").disabled) {
+ if(message !== "" && document.getElementById("connect").disabled)
+ {
   document.getElementById("send").disabled = false;
  }
- else {
+ else
+ {
   document.getElementById("send").disabled = true;
  }
 }
 
-function clearMessages() {
+function clearMessages()
+{
  document.getElementById("messages").value = "";
  document.getElementById("clear").disabled = true;
 }
 
-function onKeyPressed() {
+function onKeyPressed()
+{
  let key = window.event.keyCode;
- if(key === 17 && !document.getElementById("send").disabled) { 
-  sendMessage(); 
+ if(key === 17 && !document.getElementById("send").disabled)
+ {
+  sendMessage();
  }
 }
