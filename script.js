e3466739f3a8bbebf08fe15cf9de722b53770a0a
zyzalfors <55386215+zyzalfors@users.noreply.github.com>
2023-12-10 23:48:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,130 +1,97 @@
-let client = null;
-let topic = null;
+let client = null, topic = null;
 
-function connect()
-{
- let brokerURL = document.getElementById("brokerURL").value.trim();
- let port = parseInt(document.getElementById("port").value.trim());
- let useSSL = document.getElementById("useSSL").checked
- let reconnect = document.getElementById("reconnect").checked
- topic = document.getElementById("topic").value.trim();
- let date = new Date().toLocaleString().replace(",", "");
- if(brokerURL === "" || isNaN(port) || topic === "")
- {
-  updateMessages(date + " | Invalid broker URL, port or topic", "logs");
-  return;
- }
- updateMessages(date + " | Connecting to \"" + brokerURL + "\" on port \"" + port + "\" ...", "logs");
- client = new Paho.Client(brokerURL, port, "");
- client.onConnectionLost = onConnectionLost;
- client.onMessageArrived = onMessageArrived;
- try
- {
-  client.connect({onSuccess: onSuccess, onFailure: onFailure, useSSL: useSSL, reconnect: reconnect});
- }
- catch(error)
- {
-  updateMessages(date + " | " + error.message, "logs");
- }
+function checkText() {
+ const disabled = document.getElementById("text").value.trim().length === 0;
+ document.getElementById("send").disabled = disabled;
+ document.getElementById("retain").disabled = disabled;
 }
 
-function onSuccess()
-{
- let date = new Date().toLocaleString().replace(",", "");
- updateMessages(date + " | Subscribing to topic \"" + topic + "\" ...", "logs");
- client.subscribe(topic);
- date = new Date().toLocaleString().replace(",", "");
- updateMessages(date + " | Connected", "logs");
- document.getElementById("disconnect").disabled = false;
- document.getElementById("connect").disabled = true;
- checkText();
-}
-
-function onFailure(obj)
-{
- let date = new Date().toLocaleString().replace(",", "");
- updateMessages(date + " | " + obj.errorMessage, "logs");
-}
-
-function disconnect()
-{
- client.disconnect();
- let date = new Date().toLocaleString().replace(",", "");
- updateMessages(date + " | Disconnected", "logs");
+function afterDisconnession() {
  document.getElementById("disconnect").disabled = true;
  document.getElementById("connect").disabled = false;
+ document.getElementById("text").value = "";
+ document.getElementById("text").disabled = true;
  document.getElementById("send").disabled = true;
  document.getElementById("retain").disabled = true;
 }
 
-function onConnectionLost(message)
-{
- if(message.errorCode !== 0)
- {
-  let date = new Date().toLocaleString().replace(",", "");
-  updateMessages(date + " | Connection lost: " + message.errorMessage, "logs");
-  document.getElementById("disconnect").disabled = true;
-  document.getElementById("connect").disabled = false;
-  document.getElementById("send").disabled = true;
-  document.getElementById("retain").disabled = true;
+function connect() {
+ const brokerURL = document.getElementById("brokerURL").value.trim();
+ const port = parseInt(document.getElementById("port").value.trim());
+ const useSSL = document.getElementById("useSSL").checked;
+ const reconnect = document.getElementById("reconnect").checked;
+ topic = document.getElementById("topic").value.trim();
+ if(brokerURL.length === 0 || isNaN(port) || topic.length === 0) {
+  console.error(new Date().toISOString() + " | Invalid broker URL, port or topic");
+  return;
+ }
+ console.log(new Date().toISOString() + " | Connecting to " + brokerURL + " on port " + port + " ...");
+ try {
+  client = new Paho.Client(brokerURL, port, "");
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  client.connect({onSuccess: onSuccess, onFailure: onFailure, useSSL: useSSL, reconnect: reconnect});
+ }
+ catch(err) {
+  console.error(new Date().toISOString() + " | " + err.message);
  }
 }
 
-function onMessageArrived(message)
-{
- let date = new Date().toLocaleString().replace(",", "");
- updateMessages(date + " | " + message.destinationName + " | " + message.payloadString, "messages");
+function onSuccess() {
+ console.log(new Date().toISOString() + " | Subscribing to topic " + topic + " ...");
+ client.subscribe(topic);
+ console.log(new Date().toISOString() + " | Connected");
+ document.getElementById("disconnect").disabled = false;
+ document.getElementById("connect").disabled = true;
+ document.getElementById("text").disabled = false;
 }
 
-function updateMessages(message, textAreaID)
-{
- let textarea = document.getElementById(textAreaID);
- let messages = textarea.value !== "" ? textarea.value + "\n" : textarea.value;
- textarea.value = messages + message;
+function onFailure(err) {
+ console.error(err.errorMessage ? new Date().toISOString() + " | Failure: " + err.errorMessage : new Date().toISOString() + " | Failure");
+}
+
+function disconnect() {
+ client.disconnect();
+ console.log(new Date().toISOString() + " | Disconnected");
+ afterDisconnession();
+}
+
+function onConnectionLost(err) {
+ if(err.errorCode === 0) { return; }
+ console.error(err.errorMessage ? new Date().toISOString() + " | Connection lost: " + err.errorMessage : new Date().toISOString() + " | Connection lost");
+ afterDisconnession();
+}
+
+function onMessageArrived(mes) {
+ try {
+  const message = JSON.parse(mes.payloadString);
+  if(!message.u || !message.t) { throw new Error("Invalid message"); }
+  updateMessages(new Date().toISOString() + " | " + mes.destinationName + " | " + message.u + ":\n" + message.t);
+ }
+ catch(err) {
+  console.error(new Date().toISOString() + " | Invalid message received: " + mes.payloadString);
+ }
+}
+
+function updateMessages(message) {
+ const textarea = document.getElementById("messages");
+ textarea.value = textarea.value.length > 0 ? textarea.value + "\n" + message : message;
  textarea.scrollTop = textarea.scrollHeight;
- if(textAreaID === "messages")
- {
-  document.getElementById("clear").disabled = false;
- }
+ document.getElementById("clear").disabled = false;
 }
 
-function sendMessage()
-{
- let username = document.getElementById("username").value.trim() !== "" ? document.getElementById("username").value.trim() : "nousername";
- let message = username + ":\n" + document.getElementById("message").value.trim();
- let retain = document.getElementById("retain").checked
- document.getElementById("message").value = "";
- document.getElementById("send").disabled = true;
- document.getElementById("retain").disabled = true;
+function sendMessage() {
+ const val = document.getElementById("username").value.trim();
+ const username = val.length > 0 ? val : "none";
+ const text = document.getElementById("text").value.trim();
+ const retain = document.getElementById("retain").checked;
+ const message = JSON.stringify({u: username, t: text});
+ document.getElementById("text").value = "";
+ checkText();
  client.publish(topic, message, 1, retain);
 }
 
-function checkText()
-{
- let message = document.getElementById("message").value.trim();
- if(message !== "" && document.getElementById("connect").disabled)
- {
-  document.getElementById("send").disabled = false;
-  document.getElementById("retain").disabled = false;
- }
- else
- {
-  document.getElementById("send").disabled = true;
-  document.getElementById("retain").disabled = true;
- }
-}
-
-function clearMessages()
-{
+function clearMessages() {
  document.getElementById("messages").value = "";
  document.getElementById("clear").disabled = true;
-}
-
-function onKeyPressed()
-{
- let key = window.event.keyCode;
- if(key === 17 && !document.getElementById("send").disabled)
- {
-  sendMessage();
- }
 }
